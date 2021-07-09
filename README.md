fa47eaf1f3092b938dd3231e8b94ea0543271a7a
Katherine Aguirre <kathesama@gmail.com>
2021-07-09 16:47:39 -0300

    chore: config scripts

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # farm-mqtt-server
 Mqtt server for farm project
+
+execute 
+source ./ca_certificates.sh <host|client> 
+> source ./config.sh <dev|prod> <dockerUser> [dockerUserPassword] <host|client> [hostname] [caCertPassword] <CA_ORG> 
+
+* Values with <> are required<br>
+* Values with [] are optional but **MUST BE PROVIDED** two single quotes ''
+
+Params:
+1. 'dev|prod': This indicates which ambient will be performed on, by default is *dev*
+2. 'dockerUser': Docker user for mosquitto server, by default is *mqttAdmin*
+3. 'dockerUserPassword': Docker password for user for mosquitto server, default is *a random string* 
+4. 'host|client': This indicates which operation will be performed on, by default is *host*
+5. 'hostname': Name related with certs generated, this indicates file name and host's name
+6. 'caCertPassword': Ca Cert password related for security purposes, default is *a random string* 
+7. 'CA_ORG': this gives the remaining parameters for the CA cert config, <br>
+    > estructure **MUST BE LIKE** (default value): '/O=OwnTracks.org/OU=generate-CA/emailAddress=nobody@example.net'
---
+++ config.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+clear
+
+P_AMBIENT=${1:-dev}
+P_DOCKER_USERNAME=${2:-mqttAdmin}
+P_DOCKER_USER_KEY=${3:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)}
+P_OPTION=${4:-host}
+P_HOSTNAME=${5:-$(hostname -f)}
+P_CA_KEY=${6:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)}
+P_CA_ORG=${7:-$(echo '/O=OwnTracks.org/OU=generate-CA/emailAddress=nobody@example.net')}
+
+echo "Params received:"
+echo "Ambient: $P_AMBIENT"
+echo "Docker MQTT Server Username: $P_DOCKER_USERNAME"
+echo "Docker MQTT Username password: $P_DOCKER_USER_KEY"
+echo "Ca Cert type: $P_OPTION"
+echo "Hostname got: $P_HOSTNAME"
+printf '\e[1;32m%-6s\e[m' "Cert pass is: "
+echo "$P_CA_KEY"
+printf '\e[1;32m%-6s\e[m' "Cert pass is: "
+echo "$P_CA_KEY"
+echo "CA_ORG values got: $P_CA_ORG"
+
+#-------------------------------------------------------------------------------------------------
+printf "\n"
+printf '\e[1;31m%-6s\e[m' "Proceding to config: [$1]..."
+printf "\n"
+printf '\e[1;32m%-6s\e[m' "1 Getting mosquitto image if it not exists..."
+
+docker pull eclipse-mosquitto:latest
+sudo apt-get install mosquitto mosquitto-clients -y
+
+printf '\e[1;32m%-6s\e[m' "2 Configuring certs..."
+chmod 700 ca_certificates
+cd ca_certificates
+source ./ca_certificates.sh $P_OPTION $P_HOSTNAME $P_CA_KEY $P_CA_ORG
