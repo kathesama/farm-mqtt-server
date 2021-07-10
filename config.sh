f2f4fefc1d749463e0d896ebec9a312e3063a0f0
Katherine Aguirre <kathesama@gmail.com>
2021-07-10 15:07:42 -0300

    chore: keep creating config script

--- config.sh
+++ config.sh
@@ -30,10 +30,11 @@ printf '\e[1;32m%-6s\e[m' "1 Configuring certs..."
 echo ""
 chmod 700 ca_certificates
 cd ca_certificates
-# source ./generate-CA.sh $P_OPTION $P_HOSTNAME $P_CA_KEY $P_CA_ORG
+source ./generate-CA.sh $P_OPTION $P_HOSTNAME $P_CA_KEY $P_CA_ORG
 cd ..
 echo "1: done"
 
+#-------------------------------------------------------------------------------------------------
 printf '\e[1;32m%-6s\e[m' "2 Configuring config file..."
 echo ""
 cd config.d
@@ -41,21 +42,42 @@ sed -i "s/SERVER_NAME/$P_HOSTNAME/g" mosquitto.conf
 sed -i "s/AMBIENT/$P_AMBIENT/g" password.conf
 echo "2: done"
 
-printf '\e[1;32m%-6s\e[m' "3 Creating password file for mosquitto..."
+#-------------------------------------------------------------------------------------------------
+printf '\e[1;32m%-6s\e[m' "3 Creating and configuring password file for mosquitto..."
 echo ""
-
-# if [[ $P_AMBIENT == "dev" ]]; then  		
+touch "mosquitto-$P_AMBIENT.passwd"
 sudo mosquitto_passwd -b "mosquitto-$P_AMBIENT.passwd" $P_DOCKER_USERNAME $P_DOCKER_USER_KEY
-# else
-#     sudo mosquitto_passwd -b mosquitto-prod.passwd $P_DOCKER_USERNAME $P_DOCKER_USER_KEY
-# fi
 cd ..
 echo "3: done"
 
-
+#-------------------------------------------------------------------------------------------------
 printf '\e[1;32m%-6s\e[m' "4 Getting mosquitto image if it not exists..."
 echo ""
 
-#docker pull eclipse-mosquitto:latest
-#sudo apt-get install mosquitto mosquitto-clients -y
+docker pull eclipse-mosquitto:latest
+sudo apt-get install mosquitto mosquitto-clients -y
 echo "4: done"
+
+#-------------------------------------------------------------------------------------------------
+printf '\e[1;32m%-2s\e[m' "5 Creating docker container for mosquitto." 
+echo ""
+
+docker run --init -d \
+--name="eclipse-mosquitto" \
+--net=host \
+--restart always \
+-p 1883:1883 \
+-p 8883:8883 \
+-p 9001:9001 \
+-e "TZ=America/Argentina/Buenos_Aires" \
+-v $(pwd)/config:/mosquitto/config.d \
+-v $(pwd)/log/:/mosquitto/log \
+-v $(pwd)/data:/mosquitto/data \
+-v $(pwd)/ca_certificates/ca.crt:/ca_certificates/ca.crt \
+-v $(pwd)/ca_certificates/certs/$P_HOSTNAME.crt:/ca_certificates/server_certs/$P_HOSTNAME.crt \
+-v $(pwd)/ca_certificates/certs/$P_HOSTNAME.key:/ca_certificates/server_certs/$P_HOSTNAME.key \
+eclipse-mosquitto
+
+echo "5: done"
+
+#-------------------------------------------------------------------------------------------------
